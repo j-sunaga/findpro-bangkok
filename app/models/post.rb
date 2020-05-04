c3623ae3ad92d6b069c23aab75b76d33c675c3b3
j-sunaga <124577blue@gmail.com>
2020-05-04 13:31:57 +0900

    postモデルのバリデーション・statusのenum追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { in: 1..50 }
+  validates :detail, length: { in: 1..1000 }
+  validates :status, presence: true
+
+  enum status: { open: 0, closed: 1 }
 end
