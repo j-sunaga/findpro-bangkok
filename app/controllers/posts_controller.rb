7709d5ffe8829d2713d8ae30f7ddb772c346f2b8
j-sunaga <124577blue@gmail.com>
2020-05-05 13:46:24 +0900

    ログインユーザでヘッダーと投稿のアクション制限を設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
