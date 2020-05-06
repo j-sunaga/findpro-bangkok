4e139b8b069280a1db5f7533adc7d3a42165a9ee
j-sunaga <124577blue@gmail.com>
2020-05-06 16:44:09 +0900

    画像リサイズのメソッド定義

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -27,6 +27,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+
+  # For Profile
+  version :thumb do
+    process resize_to_fill: [100, 100, "Center"]
+  end
+
+  # For Post Show
+  version :post_detail do
+    process resize_to_fit: [300, 300]
+  end
 
   # Create different versions of your uploaded files:
   # version :thumb do
