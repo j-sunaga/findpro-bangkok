81fbc1cf967a1e545cbfec187b4ce0c7365f5220
j-sunaga <124577blue@gmail.com>
2020-05-06 16:08:37 +0900

    uploaderファイルを統一

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   validates :status, presence: true
 
   enum status: { open: 0, closed: 1 }
-  mount_uploader :post_image, PostImageUploader
+  mount_uploader :post_image, ImageUploader
 end
