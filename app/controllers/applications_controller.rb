79a2df53e6b612c0521f583646e217e161d775f5
j-sunaga <124577blue@gmail.com>
2020-05-07 16:02:18 +0900

    応募管理をするapplications_controllerと関連view,routingの設定

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationsController < ApplicationController
+  def create
+    application = current_user.applications.build(post_id: params[:post_id])
+    if application.save
+      redirect_back(fallback_location: root_path, notice: 'Successfully Applied')
+    else
+      render fallback_location
+    end
+  end
+
+  def index
+    @posts = current_user.application_posts
+  end
+
+  def destroy
+    application = current_user.applications.find(params[:id])
+    if application.destroy!
+      redirect_back(fallback_location: root_path, notice: 'Un-Applied')
+    else
+      render fallback_location
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :bookmarks, only: [:index, :create, :destroy]
+  resources :applications, only: [:index, :show, :create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
