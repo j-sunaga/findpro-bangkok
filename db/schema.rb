d3934e0ea72207900983be19c971dfc68fbd756d
j-sunaga <124577blue@gmail.com>
2020-05-07 15:22:08 +0900

    応募情報を管理するapplicationテーブル・モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_035748) do
+ActiveRecord::Schema.define(version: 2020_05_07_062111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
---
+++ spec/factories/applications.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :application do
+    post_id { 1 }
+    user_id { 1 }
+  end
+end
