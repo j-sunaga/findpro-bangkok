f7cc6bb34bc856ac9959f348449d1953f9cab7ec
j-sunaga <124577blue@gmail.com>
2020-05-09 15:47:50 +0900

    commentモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_081442) do
+ActiveRecord::Schema.define(version: 2020_05_09_064439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 2020_05_07_081442) do
     t.index ["user_id", "category_id"], name: "index_category_users_on_user_id_and_category_id", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content", limit: 1000, default: "", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title", limit: 50, default: "", null: false
     t.text "detail"
@@ -86,5 +96,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_081442) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users", column: "recruiter_id"
 end
