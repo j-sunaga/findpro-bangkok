59154da5d6be83a59046d7d30c9452587c6ed5e3
j-sunaga <124577blue@gmail.com>
2020-05-13 13:31:12 +0900

    postモデルテストの記載方法の統一

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,11 +10,23 @@ RSpec.describe '募集機能', type: :model do
   end
   it 'detailが1001文字以上ならバリデーションが通らない' do
     post = build(:post, detail: 'a'*1001)
-    expect(post).not_to be_valid
+    expect(post).to be_invalid
+  end
+  it 'deadlineがnilならバリデーションが通らない' do
+    post = build(:post, deadline: nil)
+    expect(post).to be_invalid
+  end
+  it 'deadlineが過去の日付ならバリデーションが通らない' do
+    post = build(:post, deadline: DateTime.now - 1.week)
+    expect(post).to be_invalid
   end
   it 'statusがnilならバリデーションが通らない' do
     post = build(:post, status: nil)
-    expect(post).not_to be_valid
+    expect(post).to be_invalid
+  end
+  it 'statusがnilならバリデーションが通らない' do
+    post = build(:post, status: nil)
+    expect(post).to be_invalid
   end
   it 'titleが50文字/detailが1000字ならバリデーションが通る' do
     post = build(:post, title: 'a'*50, detail: 'a'*1000)
