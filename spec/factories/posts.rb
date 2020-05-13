fac7a16e66455a5732668ffd43e31ff3731183e4
j-sunaga <124577blue@gmail.com>
2020-05-13 13:31:43 +0900

    postモデルのfactorybotにアソシエーションの追加と他修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,15 +1,19 @@
 FactoryBot.define do
   factory :post do
+
     sequence :title do |n|
-      "post_#{1}"
+      "post_#{n}"
     end
 
     sequence :detail do |n|
       "post_#{n}_detail"
     end
     post_image{"no_image.jpg"}
-    deadline { 1.month.ago }
+    deadline { DateTime.now + 1.week }
     status { :open }
+
+    association :recruiter, factory: :user
+    association :selected_user, factory: :user
 
   end
 end
