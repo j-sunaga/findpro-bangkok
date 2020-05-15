b27dac2bb57a5dbd3a747b4c45d3d717b70deb4c
j-sunaga <124577blue@gmail.com>
2020-05-15 12:00:49 +0900

    conversation_id,user_idのnilチェックを外す

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
-  validates_presence_of :body, :conversation_id, :user_id
+  validates_presence_of :body
   validates :body, length: { in: 1..255 }
 
   def message_time
