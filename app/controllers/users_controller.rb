6adf7c46f6d4abf3e90909a3a6db682321470327
j-sunaga <124577blue@gmail.com>
2020-05-29 12:36:55 +0900

    applicant indexからprofessionalにアクション名,ビュー名を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show myprofile]
 
   def show
-  def applicant_index
+    @conversation = Conversation.between(@user, current_user)
+  end
+
+  def professional
     @users = User.page(params[:page]).where(applicant_or_recruiter: 'applicant')
     @categories = Category.all
   end
 
+  def myprofile; end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
-
 end
