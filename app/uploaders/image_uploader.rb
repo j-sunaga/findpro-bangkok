29231bb1619065dc890c8fa9f7c713c4973d1e70
j-sunaga <124577blue@gmail.com>
2020-06-05 13:05:41 +0900

    画像が無い場合に表示するno_image画像の追加とimage_uploadの設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,12 +14,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*_args)
+    #   #   # For Rails 3.1+ asset pipeline compatibility:
+    'assets/no_image.jpg'
+    #   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    #   #
+    #   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -30,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # For Profile
   version :thumb do
-    process resize_to_fill: [100, 100, "Center"]
+    process resize_to_fill: [100, 100, 'Center']
   end
 
   # For Post Show
