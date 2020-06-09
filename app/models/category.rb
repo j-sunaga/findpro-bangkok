ec61f88f0a8ca8154b4310d8bb6d68537c2233a1
j-sunaga <124577blue@gmail.com>
2020-06-09 16:41:10 +0900

    categoryモデルの名前の一意制約を追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
   has_many :users, through: :category_users, source: :user
   has_many :category_posts, dependent: :destroy
   has_many :posts, through: :category_posts, source: :post
+  validates :name, uniqueness: true
 
   scope :select_category, ->(category) { where('categories.name = ?', category) }
 end
