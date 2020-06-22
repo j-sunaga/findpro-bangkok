a75c9ca159b1552eb5cc36b5dbc600117c017424
j-sunaga <124577blue@gmail.com>
2020-06-22 20:02:22 +0900

    professional検索機能のためのuserシステムテストへの追記

--- spec/factories/category_users.rb
+++ spec/factories/category_users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category_user do
-    user_id { 1 }
-    category_id { 1 }
+    user {}
+    association :category
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -46,4 +46,60 @@ RSpec.describe 'ユーザ管理機能', type: :system do
       end
     end
   end
+  describe '応募者一覧機能#professional' do
+    context '投稿者でログインした場合' do
+      it '応募者一覧が表示される' do
+        act_as recruiter do
+          visit professional_users_path
+          expect(page).to have_content applicant.name
+        end
+      end
+    end
+    context '複数の応募者を作成した場合' do
+      let!(:new_applicant) { create(:applicant) }
+      it '応募者の登録日が新しい順に並んでいる' do
+        act_as recruiter do
+          visit professional_users_path
+          user_list = all('.user_name')
+          expect(user_list[0]).to have_content new_applicant.name
+          expect(user_list[1]).to have_content applicant.name
+        end
+      end
+    end
+  end
+  describe '応募者検索機能#professional' do
+    context '検索をした場合' do
+      let!(:applicants) { create_list(:applicant, 2) }
+      let!(:category_user1) { create(:category_user, user: applicants[0]) }
+      let!(:category_user2) { create(:category_user, user: applicants[1]) }
+      it '名前で検索できる' do
+        act_as recruiter do
+          visit professional_users_path
+          fill_in 'q[name_or_content_cont]', with: applicants[0].name
+          click_button 'Search'
+          expect(page).to have_content applicants[0].name
+          expect(page).to_not have_content applicants[1].name
+        end
+      end
+      it 'カテゴリで検索できる' do
+        act_as recruiter do
+          visit professional_users_path
+          select applicants[0].categories.first.name.to_s, from: 'category'
+          click_button 'Search'
+          expect(page).to have_content applicants[0].name
+          expect(page).to_not have_content applicants[1].name
+        end
+      end
+      it '名前とカテゴリで検索できる' do
+        act_as recruiter do
+          visit professional_users_path
+          fill_in 'q[name_or_content_cont]', with: applicants[0].name
+          select applicants[0].categories.first.name.to_s, from: 'category'
+          click_button 'Search'
+          expect(page).to have_content applicants[0].name
+          expect(page).to_not have_content applicants[1].name
+        end
+      end
+    end
+  end
 end
