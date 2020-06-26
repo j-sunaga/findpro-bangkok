9e4eb475d7a51e30a2f533be2de5b980bc7f55aa
j-sunaga <124577blue@gmail.com>
2020-06-26 17:36:55 +0900

    コメントの文字数制限を255文字までに変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  belongs_to :parent,  class_name: "Comment", optional: true
-  has_many   :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent,  class_name: 'Comment', optional: true
+  has_many   :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
 
-  validates :content, presence: true, length: { in: 1..1000 }
+  validates :content, presence: true, length: { in: 1..255 }
 end
