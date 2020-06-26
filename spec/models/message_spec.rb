d12c78903a779bd4fc385d880692ab46490bcb42
j-sunaga <124577blue@gmail.com>
2020-06-26 18:31:47 +0900

    messageモデルのモデルテストの修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
+  let!(:user) { create(:applicant) }
   it 'bodyが255文字より大きいならバリデーションが通らない' do
-    message = build(:message, body:'a'*256 )
+    message = build(:message, body: 'a' * 256, user: user)
     expect(message).to be_invalid
   end
   it 'bodyが空白ならバリデーションが通らない' do
-    message = build(:message, body:'' )
+    message = build(:message, body: '', user: user)
     expect(message).to be_invalid
   end
   it 'bodyが255文字以内ならバリデーションが通る' do
-    message = build(:message, body:'a'*255 )
+    message = build(:message, body: 'a' * 255, user: user)
     expect(message).to be_valid
   end
 end
