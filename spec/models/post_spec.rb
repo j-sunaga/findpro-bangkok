4893c66905756879bf5cedf057e58b9edaeae87c
j-sunaga <124577blue@gmail.com>
2020-06-26 18:36:59 +0900

    投稿モデルのモデルテストで重複しているstatusバリデーションテストを削除

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 RSpec.describe '募集機能', type: :model do
   it 'titleが空ならバリデーションが通らない' do
-    post = build(:post, title:'')
+    post = build(:post, title: '')
     expect(post).to be_invalid
   end
   it 'titleが51文字以上ならバリデーションが通らない' do
-    post = build(:post, title: 'a'*51)
+    post = build(:post, title: 'a' * 51)
     expect(post).to be_invalid
   end
   it 'detailが1001文字以上ならバリデーションが通らない' do
-    post = build(:post, detail: 'a'*1001)
+    post = build(:post, detail: 'a' * 1001)
     expect(post).to be_invalid
   end
   it 'deadlineがnilならバリデーションが通らない' do
@@ -24,12 +24,8 @@ RSpec.describe '募集機能', type: :model do
     post = build(:post, status: nil)
     expect(post).to be_invalid
   end
-  it 'statusがnilならバリデーションが通らない' do
-    post = build(:post, status: nil)
-    expect(post).to be_invalid
-  end
   it 'titleが50文字/detailが1000字ならバリデーションが通る' do
-    post = build(:post, title: 'a'*50, detail: 'a'*1000)
+    post = build(:post, title: 'a' * 50, detail: 'a' * 1000)
     expect(post).to be_valid
   end
 end
