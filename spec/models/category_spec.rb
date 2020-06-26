733e16a65bec40df6f705c8a4af56702e7be6b87
j-sunaga <124577blue@gmail.com>
2020-06-26 17:24:05 +0900

    categoryモデルのモデルテストの作成

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
-
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'カテゴリ機能', type: :model do
+  it 'nameが空ならバリデーションが通らない' do
+    category = build(:category, name: '')
+    expect(category).to be_invalid
+  end
+  it 'nameが51文字以上ならバリデーションが通らない' do
+    category = build(:category, name: 'a' * 51)
+    expect(category).to be_invalid
+  end
+  it '同じnameのカテゴリがある場合,バリデーションが通らない' do
+    create(:category, name: 'category')
+    category = build(:category, name: 'category')
+    expect(category).to be_invalid
+  end
+  it 'nameが50文字以内ならバリデーションが通る' do
+    category = build(:category, name: 'a' * 50)
+    expect(category).to be_valid
+  end
 end
