41ba8c020557c0fba57754150fef93495a936ec3
j-sunaga <124577blue@gmail.com>
2020-06-26 17:21:54 +0900

    モデルバリデーションの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category < ApplicationRecord
   has_many :category_posts, dependent: :destroy
   has_many :posts, through: :category_posts, source: :post
   validates :name, uniqueness: true
+  validates :name, presence: true, length: { in: 1..50 }
 
   scope :select_category, ->(category) { where('categories.name = ?', category) }
 end
