2ed3edae7c6ea608ecb781db4affc154e90ec522
j-sunaga <124577blue@gmail.com>
2020-06-26 18:06:53 +0900

    commentモデルのモデルテストの作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'コメント機能', type: :model do
+  let!(:user) { create(:applicant) }
+  let!(:post) { create(:post) }
+  it 'contentが空ならバリデーションが通らない' do
+    comment = build(:comment, content: '', user: user, post: post)
+    expect(comment).to be_invalid
+  end
+  it 'contentが256文字以上ならバリデーションが通らない' do
+    comment = build(:comment, content: 'a' * 256, user: user, post: post)
+    expect(comment).to be_invalid
+  end
+  it 'contentが255文字以内ならバリデーションが通る' do
+    comment = build(:comment, content: 'a' * 255, user: user, post: post)
+    expect(comment).to be_valid
+  end
 end
