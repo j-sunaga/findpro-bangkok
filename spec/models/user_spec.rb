fceeaf627ba69562a522ab3e88c7bb326233603b
j-sunaga <124577blue@gmail.com>
2020-06-26 18:41:45 +0900

    userモデルのファクトリ名をapplicantに変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,44 +2,44 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'nameが空ならバリデーションが通らない' do
-    user = build(:user, name: '')
-    expect(user).to be_invalid
+    applicant = build(:applicant, name: '')
+    expect(applicant).to be_invalid
   end
   it 'nameが51文字以上ならバリデーションが通らない' do
-    user = build(:user, name: 'a' * 51)
-    expect(user).to be_invalid
+    applicant = build(:applicant, name: 'a' * 51)
+    expect(applicant).to be_invalid
   end
   it 'emailが空ならバリデーションが通らない' do
-    user = build(:user, email: '')
-    expect(user).not_to be_valid
+    applicant = build(:applicant, email: '')
+    expect(applicant).to be_invalid
   end
   it 'emailフォーマットが適切でない場合、バリデーションが通らない' do
-    user = build(:user, email: 'aaaaa.com')
-    expect(user).not_to be_valid
+    applicant = build(:applicant, email: 'aaaaa.com')
+    expect(applicant).to be_invalid
   end
   it '同じemailの場合,バリデーションが通らない' do
-    create(:user, email: 'b' * 50 + '@example.com')
-    user2 = build(:user, email: 'b' * 50 + '@example.com')
-    expect(user2).not_to be_valid
+    create(:applicant, email: 'b' * 50 + '@example.com')
+    applicant2 = build(:applicant, email: 'b' * 50 + '@example.com')
+    expect(applicant2).to be_invalid
   end
   it 'passwordが空ならバリデーションが通らない' do
-    user = build(:user, password: '')
-    expect(user).not_to be_valid
+    applicant = build(:applicant, password: '')
+    expect(applicant).to be_invalid
   end
   it 'passwordが6文字未満ならバリデーションが通らない' do
-    user = build(:user, password: '12345')
-    expect(user).not_to be_valid
+    applicant = build(:applicant, password: '12345')
+    expect(applicant).to be_invalid
   end
   it 'contentが1001文字以上ならバリデーションが通らない' do
-    user = build(:user, content: 'a' * 1001)
-    expect(user).not_to be_valid
+    applicant = build(:applicant, content: 'a' * 1001)
+    expect(applicant).to be_invalid
   end
   it 'applicant_or_recruiterがnilならバリデーションが通らない' do
-    user = build(:user, applicant_or_recruiter: nil)
-    expect(user).not_to be_valid
+    applicant = build(:applicant, applicant_or_recruiter: nil)
+    expect(applicant).to be_invalid
   end
   it 'nameが50文字/contentが1000字ならバリデーションが通る' do
-    user = build(:user, name: 'a' * 50, content: 'a' * 1000)
-    expect(user).to be_valid
+    applicant = build(:applicant, name: 'a' * 50, content: 'a' * 1000)
+    expect(applicant).to be_valid
   end
 end
