af4eff783fb983b1eb60ad8fbffdecbdf10d787a
j-sunaga <124577blue@gmail.com>
2020-08-16 11:33:30 +0900

    production環境用のpuma設定ファイルを作成

---
+++ config/puma/production.rb
@@ -0,0 +1,11 @@
+environment "production"
+
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads threads_count, threads_count
+port        ENV.fetch('PORT') { 3000 }
+plugin :tmp_restart
+
+app_root = File.expand_path('..', __dir__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
